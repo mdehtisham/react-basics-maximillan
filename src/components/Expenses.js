424534521df0903ad73bd5bcafd8c5b5ebf59f48
mdehtisham <mefxe01@gmail.com>
2022-01-10 23:52:03 +0530

    added the style for expenses component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,10 @@
 import ExpenseItem from "./ExpenseItem"
+import './Expenses.css'
 
 function Expenses(props){
 
     return (
-        <div>
+        <div className="expenses">
             {props.expenses.map(expense => {
                 return  <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date} key={expense.id}></ExpenseItem>
             })}
