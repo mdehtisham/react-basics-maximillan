e72ebc9ea0d05e590246fa28f1419f2e8773d739
mdehtisham <mefxe01@gmail.com>
2022-01-11 23:19:43 +0530

    learned to used events and basic use of useState

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,20 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
+import { useState } from 'react';
 
 
 
 function ExpenseItem(props){
-    console.log(props)
+    const [title, setTitle] = useState(props.title)
+    function handleClick(){
+        setTitle('Updated')
+    }
     return (<div className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
+            <button onClick={handleClick}>change title</button>
         </div>
     </div>)
 }
