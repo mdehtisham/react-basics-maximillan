864c4caff27a4c61a3f9b392a27ae36c365cf63a
mdehtisham <mefxe01@gmail.com>
2022-01-11 23:43:58 +0530

    added newExpense and ExpenseForm component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import { useState } from 'react';
 
 
 
 function ExpenseItem(props){
-    const [title, setTitle] = useState(props.title)
-    function handleClick(){
-        setTitle('Updated')
-    }
+   
     return (<div className='expense-item'>
         <ExpenseDate date={props.date}></ExpenseDate>
         <div className='expense-item__description'>
-            <h2>{title}</h2>
+            <h2>{props.title}</h2>
             <div className='expense-item__price'>${props.amount}</div>
-            <button onClick={handleClick}>change title</button>
         </div>
     </div>)
 }
